08ab7a1605ddfc8a768deeec2aeaa79feb7caec4
Alex <alexpark@pursuit.org>
2019-04-11 16:27:26 -0400

    add questions reducer to root

--- frontend/src/redux/reducers/RootReducer.js
+++ frontend/src/redux/reducers/RootReducer.js
@@ -5,12 +5,13 @@ import {combineReducers} from 'redux'
 //Import Reducers from 
 import loginReducer from "./login_logout"
 import checkUserAuthStatus_reducer from "./checkAuth_Reducer"
-
+import questionsReducer from './questionsReducer'
 
 //Combine all Reducers into one for use. 
 const RootReducer = combineReducers({
   LoginState: loginReducer,
   CurrentAutState: checkUserAuthStatus_reducer,
+  questions: questionsReducer
 })
 
 export default RootReducer
