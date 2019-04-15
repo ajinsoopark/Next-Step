02f000e6d087697b4dcf69c20804f1ce7f4add4b
Alex <alexpark@pursuit.org>
2019-04-15 12:36:02 -0400

    random question

--- frontend/src/Component/Dashboard/dashboard.jsx
+++ frontend/src/Component/Dashboard/dashboard.jsx
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import Progress from '../Progress/progress'
 import DashSplash from './dashSplash'
+import RandomQuestion from './randomQuestion'
 
 import './dashboard.css'
 
 class Dashboard extends Component {
-
+    constructor () {
+        super ()
+        this.state = {
+            questionId: '',
+            categoryId: '',
+            questionBody: '',
+            category: ''
+        }
+    }
     componentDidMount () {
        this.props.function_checkStatus()
+       axios.get('/questions/random')
+       .then(res => {
+           let randomQuestion = res.data.question[0]
+           this.setState({
+               questionId: randomQuestion.id,
+               categoryId: randomQuestion.category_id,
+               questionBody: randomQuestion.question_body,
+               category: randomQuestion.category
+           })
+       })
     }
 
     render () {
-        console.log(this.state)
+        const { questionId, categoryId, questionBody, category } = this.state
         return (
             <div className='dashboardContainer'>
-                <DashSplash userName={ this.props.state.CurrentAutState.username }/>
+                <div className='splashAndQuestion'>
+                    <DashSplash userName={ this.props.state.CurrentAutState.username }/>
+                    <RandomQuestion 
+                     questionId={questionId}
+                     categoryId={categoryId}
+                     questionBody={questionBody}
+                     category={category}/>
+                </div>
                 <Progress/>
             </div>
         )
