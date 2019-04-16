2d0730fb4271fe9d83877c4c5991becc19743087
JJ <jackyong@pursuit.org>
2019-04-16 10:41:28 -0400

    Question tabs

--- frontend/src/Component/Question/selector.js
+++ frontend/src/Component/Question/selector.js
@@ -2,30 +2,36 @@ import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 
-
-const Selection = (props) =>{
-  return(
-  <>
-  <Tabs selectedIndex={props.tabIndex} onSelect={tabIndex => props.TabSelectedChange(tabIndex)}>
-
-  <TabList> 
-  <Tab> Answers </Tab>
-  <Tab> Reviews </Tab>
-  </TabList>
-
-  <TabPanel> 
-  <h1> THis is 1</h1>
-  </TabPanel>
+class Selection extends Component {
+constructor (props) {
+  super(props)
   
-  <TabPanel> 
-  <h1> THis is 2</h1>
-  </TabPanel>
-  
-  
-  </Tabs>
-  </>
-  )
+}
 
+
+  render () {
+    return (
+        <>
+          <Tabs selectedIndex={this.props.tabIndex} onSelect={tabIndex => this.props.TabSelectedChange(tabIndex)}>
+
+          <TabList> 
+          <Tab> Answers </Tab>
+          <Tab> Reviews </Tab>
+          </TabList>
+
+          <TabPanel> 
+          <h1> THis is 1</h1>
+          </TabPanel>
+          
+          <TabPanel> 
+          <h1> THis is </h1>
+          </TabPanel>
+          
+          
+          </Tabs>
+        </>
+    )
+  }
 }
 
 export default Selection
