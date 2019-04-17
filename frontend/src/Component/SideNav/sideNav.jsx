e634859d1bd0e9e04f1c2c18bc07571c10375f84
ThaiSonP <tysonpan@pursuit.org>
2019-04-17 18:59:03 -0400

    minor error

--- frontend/src/Component/SideNav/sideNav.jsx
+++ frontend/src/Component/SideNav/sideNav.jsx
@@ -43,15 +43,16 @@ class SideNavMenu extends Component {
                     eventKey='leaderboard'
                     active={activeTab === 'leaderboard' ? true : false}>
                         <NavIcon>
-                            <NavLink className='sideNavLink' to={'/leaderboard'}>
+                            <NavLink className='sideNavLink' to={'/leaderboard'}/>
                                 <div className='sideBarIcon'>
                                     {/* {leaderboardIcon} */}
                                     <img src = {leaderboardIcon2} alt = "Circled Q" width= "35" />
 
                                 </div>
+
                         </NavIcon>
                         <NavText>
-                            <NavLink className='sideNavLink' to={'/leaderboard'}>
+                            <NavLink className='sideNavLink' to={'/leaderboard'}/>
                                 Leaderboard
                         </NavText>
                     </NavItem>
