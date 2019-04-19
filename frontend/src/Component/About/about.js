0d47d8cfa69ed9ef770434e12de24b9b2092106c
Alex <alexpark@pursuit.org>
2019-04-19 07:32:02 -0400

    styling on answers

--- frontend/src/Component/About/about.js
+++ frontend/src/Component/About/about.js
@@ -53,32 +53,32 @@ developers_map = (developers) =>{
     return (
       <>
   <div className = "about_component" >
-        <div className = "about_page_titles" >
-          <h1>  Welcome to Next Step, the site where you hone your skills. </h1>
+        <div className = "aboutHeader" >
+          Welcome to Next Step, the site where you hone your skills
+      </div>
+    <div className='aboutBodyContainer'>  
+      <div className = "about_body">
+        <div className = "about_body_headers">
+        <h1> The Platform For You </h1>
+        <p> Next Step is for job-seekers that would like know what questions they should be prepare for. A hub for job-seekers to share their interview answers, and to recieve feedback from other job-seekers to prepare for their next interview!</p>
+        </div>
       </div>
 
     <div className = "about_body">
-      <div className = "about_body_headers">
-      <h1> The Platform For You </h1>
-      <p> Next Step is for job-seekers that would like know what questions they should be prepare for. A hub for job-seekers to share their interview answers, and to recieve feedback from other job-seekers to prepare for their next interview!</p>
+        <div className = "about_body_headers">
+        <h1> Practice </h1>
+        <p> Next Step helps you keep track of how many questions you answer. You can watch your progress grow.
+        Rather than just reading a questions, Next Step allows you to hear the question like a real interview. Prepare now, and be ready for your future interview. </p>
+        </div>
       </div>
-    </div>
 
-   <div className = "about_body">
-      <div className = "about_body_headers">
-      <h1> Practice </h1>
-      <p> Next Step helps you keep track of how many questions you answer. You can watch your progress grow.
-      Rather than just reading a questions, Next Step allows you to hear the question like a real interview. Prepare now, and be ready for your future interview. </p>
+    <div className = "about_body">
+        <div className = "about_body_headers">
+        <h1> Feedback </h1>
+        <p> Collective reviews and construtive feedback from  interview professionals, field experts, and fellow job-seekers. Next Step gives you an open space for you to sharpen and deliver your interview answers.  </p>
+        </div>
       </div>
-    </div>
-
-   <div className = "about_body">
-      <div className = "about_body_headers">
-      <h1> Feedback </h1>
-      <p> Collective reviews and construtive feedback from  interview professionals, field experts, and fellow job-seekers. Next Step gives you an open space for you to sharpen and deliver your interview answers.  </p>
-      </div>
-    </div>
-
+    </div>  
       <div className = "about_body_developers">
 
         <div className = "about_page_titles" >
