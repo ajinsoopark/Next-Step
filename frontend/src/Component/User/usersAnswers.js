452007655d8b2b53107c3c8e1e64526d0877fa02
ThaiSonP <tysonpan@pursuit.org>
2019-04-23 17:08:03 -0400

    sylstic changes to login & sign up

--- frontend/src/Component/User/usersAnswers.js
+++ frontend/src/Component/User/usersAnswers.js
@@ -25,10 +25,8 @@ const disLikes= (loggedInUser,answer_id,getData)=>{
 }
 
 const UsersAnswers = ({ data, likes, loggedInUser, getData }) => {
-  console.log(data)
+  // console.log(data)
   console.log(likes)
-
-
 
     if(data && likes){
     let likeThings=likes.map(a => a.answer_id)
