ceb219431516b1757dcf96d100c397044b69196f
Alex <alexpark@pursuit.org>
2019-04-25 18:14:15 -0400

    changes to connect to db

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')({});
-const connectionString = process.env.DATABASE_URL
+const connectionString = (process.env.DATABASE_URL || 'postgres://localhost:5432/nextstep')
 // const db = pgp('postgres://localhost:5432/nextstep');
 const db = pgp(connectionString)
 
