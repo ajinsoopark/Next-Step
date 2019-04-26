b6f5eb5e6c641e02e42a4f4135fb2b80096f2bbd
Alex <alexpark@pursuit.org>
2019-04-26 17:10:50 -0400

    changes9000

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.use(express.static(path.join(__dirname, '/../frontend/build/index.html')));
 app.use(session({
     secret: "NextStepToTheNextStage",
     resave: false,
@@ -39,9 +39,6 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(cookieParser("NextStepToTheNextStage"));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
-});
 
 // app.use('/*', indexRouter);
 app.use('/users', usersRouter);
@@ -54,9 +51,9 @@ app.use('/likes', likesRouter);
 app.use('/feedbacks',feedbacksRouter);
 app.use('/', indexRouter);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
