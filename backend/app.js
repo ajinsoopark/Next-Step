aeb44c8df480a17fb8ab6e7a1443597b1bdc8562
Alex <alexpark@pursuit.org>
2019-04-26 17:15:29 -0400

    changes to pathname

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/../frontend/build/index.html')));
+app.use(express.static(path.join(__dirname + '/../frontend/build/')));
 app.use(session({
     secret: "NextStepToTheNextStage",
     resave: false,
