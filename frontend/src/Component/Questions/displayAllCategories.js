7aeb921afcc3d5022126b8a17f423690b40a6512
Alex <alexpark@pursuit.org>
2019-04-30 17:59:12 -0400

    tab component used to choose through question categories

--- frontend/src/Component/Questions/displayAllCategories.js
+++ frontend/src/Component/Questions/displayAllCategories.js
@@ -1,25 +1,55 @@
 import React from "react";
 import DisplayQuestions from "./displayQuestions"
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 import "./categories.css"
 import "./questions.css"
 
-const DisplayAllCategories = ({ userAnsweredList,allCategories, allQuestions, selectedCategory, handleChange }) => {
-    let categoryList = allCategories.map((category, i) => {
-        return(
-                <button key = {i} className = "allButtons" type="button" onClick={handleChange} value={category.category}>{category.category}</button>
+const DisplayAllCategories = ({ userAnsweredList,allCategories, allQuestions, categoryIndex, handleChange, selectedCategory }) => {
+    // let categoryList = allCategories.map((category, i) => {
+    //     return(
+    //             <button key = {i} className = "allButtons" type="button" onClick={handleChange} value={category.category}>{category.category}</button>
+    //     )
+    // })
+    
+    const categoryTabs = allCategories.map(categoryObj => {
+        return <Tab key={categoryObj.id}>{categoryObj.category}</Tab>
+    })
+
+    const categoryQuestions = allCategories.map(categoryObj => {
+        return (
+            <TabPanel key={categoryObj.id}>
+                <DisplayQuestions 
+                 userAnsweredList={userAnsweredList} 
+                 allQuestions={allQuestions} 
+                 selectedCategory={selectedCategory}/>
+            </TabPanel>
         )
     })
+    
+    console.log(allCategories)
 
     return(
         <div>
-            <div className="categoryList">
-                {categoryList}
-            </div>
-            <div className="displayQuestions">
+            {/* <div className="categoryList"> */}
+            <Tabs selectedIndex={categoryIndex} onSelect={tabIndex => handleChange(tabIndex)}>
+            
+                <TabList>
+                    {categoryTabs}
+                </TabList>
+                <div className='displayQuestions'>
+                    <div className='displayQuestionsChild'>
+                        {categoryQuestions}
+                    </div>
+                </div>
+            </Tabs>
+
+            {/* </div> */}
+            {/* <div className="displayQuestions">
                 <div className="displayQuestionsChild">
                     <DisplayQuestions userAnsweredList = {userAnsweredList} allQuestions={allQuestions} selectedCategory={selectedCategory} />
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
