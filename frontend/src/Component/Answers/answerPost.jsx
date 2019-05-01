33533f7b4a7701350895db612a4973eb5db0b099
Alex <alexpark@pursuit.org>
2019-05-01 17:16:27 -0400

    add feedback does not appear on the current users answers

--- frontend/src/Component/Answers/answerPost.jsx
+++ frontend/src/Component/Answers/answerPost.jsx
@@ -3,27 +3,16 @@ import React, { Component } from 'react'
 import Feedback from '../Feedback/container'
 
 class AnswerPost extends Component {
-    constructor () {
-        super ()
-        this.state = {
-
-        }
-    }
 
     render () {
-        const { id, answer_body, question_body, category, like_count, sortIndex, categoryIndex } = this.props
-
+        const { id, answer_body, question_body, category, user_id } = this.props
+        console.log(this.props)
         return (
             <div className='answerQuestionPostContainer'>
                 <div className='userCategoryLikes'>
                     <div className='categoryContainer'>
                         {category}
                     </div>
-                    {/* <div className='answerLikeContainer'>
-                        <div className='likeText'>
-                        {`${like_count} ${parseInt(like_count) === 1 ? 'like' : 'likes'}`}
-                        </div>
-                    </div> */}
                 </div>
                 <div className='answerQuestion'>
                     { question_body }
@@ -32,7 +21,8 @@ class AnswerPost extends Component {
                     {answer_body}
                 </div>
                 <Feedback 
-                 answer_id={id}/>
+                 answer_id={id}
+                 user_id={user_id}/>
             </div>
         )
     }
