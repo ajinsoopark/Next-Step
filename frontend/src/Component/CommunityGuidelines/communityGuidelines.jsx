f2405cb1fd614ab01e7f7c81e609b5e27c95f485
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-05-09 16:57:26 -0400

    adds guidelines

--- frontend/src/Component/CommunityGuidelines/communityGuidelines.jsx
+++ frontend/src/Component/CommunityGuidelines/communityGuidelines.jsx
@@ -1,12 +1,59 @@
 import React from 'react'
+import './community.css'
 
 const CommunityGuidelines = () => {
 
     return (
         <div className='guideParent'>
-                <div className='guide'>
-                Community Guidelines
+                <div className='guideTitle'>
+                    Community Guidelines
                 </div>
+                <div className='guideIntro'>
+                When you use Next Step, you join a community of professionals looking to improve on their interview skills by practicing answering various interview questions and giving as well as getting valuable feedback to those answers. We trust you to be courteous and professional when creating content on this platform, but just in case we’ve put together some general guidelines:
+                </div>
+                <div className='unorderedGuide'>
+                    <ul>
+                        <li>
+                            - We do not support content that promotes or condones violence against individuals or groups based on race or ethnic origin, religion, disability, gender, age, nationality, veteran status, or sexual orientation/gender identity, or whose primary purpose is inciting hatred on the basis of these core characteristics. 
+                        </li>
+                        <li>
+                            - It’s not ok to post abusive comments. Cyber-bullying is not tolerated on Next Step and users violating this rule will be removed from the platform.
+                        </li>
+                        <li>
+                            - Please be constructive and supportive when offering feedback to other users on their answers. Constructive criticism done right, leaves the receiver excited to make revisions to their story, rather than feeling shattered.
+                        </li>
+                
+                    </ul>
+
+                    <div className='constructiveIntro'>
+                    If you are not sure how to be constructive when giving feedback please refer to these guiding principles:
+                    </div>
+                    <div className='orderedGuide'> 
+                        <ol>
+                            <li>
+                            1. Be specific.
+                            </li>
+                            <li>
+                            2. Use the STAR method! STAR is an acronym for Situation, Task, Action, Result. While this method is great for answering questions it’s just as good for giving feedback! 
+                            </li>
+                            <li>
+                            3. Be descriptive, not accusative
+                            </li>
+                            <li>
+                            4. Provide actionable information 
+                            </li>
+                            <li>
+                            5. Formulate positively  
+                            </li>
+                            <li>
+                            6. Don’t be shy to compliment the user’s answer if you think it’s great and there are no improvements needed!
+                            </li>
+                        
+                        </ol>
+                    </div>
+
+                </div>
+
         </div>
     )
 }
--- frontend/src/Component/CommunityGuidelines/star.jsx
+++ frontend/src/Component/CommunityGuidelines/star.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
+import "./star.css"
 
 const StarAnswer = () => {
 
     return (
-        <div className='guideParent'>
-                <div className='guide'>
-                STAR
-                </div>
-        </div>
+        <div className='starParent'>
+            <div className='starTitle'>
+                 STAR Method
+            </div>
+            <div>
+                <ul>
+                    <li>
+                <a className='liTitle'>Situation.</a>Open with a brief description of the context of the success story (who, what, where, when, how).
+                    </li>
+                    <li>
+                    <a className='liTitle'>Task.</a>Explain the task you had to complete, highlighting any specific challenges or constraints (e.g. deadlines, limited resources).
+                    </li>
+                    <li>
+                    <a className='liTitle'>Action.</a>Describe the specific actions that you took to complete the task. These should highlight desirable traits without needing to state them (initiative, intelligence, dedication, technical skills, teamwork, etc.).
+                    </li>
+                    <li>
+                    <a className='liTitle'>Result.</a>Close with the result of your efforts. Quantify the result when possible (e.g. “this optimization resulted in a 15% increase in views”).
+                    </li>
+                </ul>
+            </div>
+            <div>
+            Examples: 
+            <h1>
+            Can you detail a mistake you made and how you reacted to it?
+            </h1>
+            STAR Model Answer: One time, I switched the packing labels of two packages I sent to customers. I had to correct the problem without angering the clients. I called them both up and provided them with shipping labels with the correct addresses while offering each of them a small gift certificate to use on a future order. The customers not only helped me fix my mistake, but they were both satisfied and will return and shop again.
+            </div>
+            <div>
+            Tell me about a time when you performed well under enormous pressure.
+            STAR Model Answer: At my last job, my coworker needed to miss work for some time, and their project was left unfinished and without a manager. My supervisor instructed me to take on the project, and with no leniency on the deadline, I had days to complete a project that originally should have taken several weeks. I requested and was granted reduced weekly goals, giving me more time to finish the special project. As far as my weekly goals, I was able to delegate them out to teammates. With my reduced goals, I dedicated more time to the special project. This allowed me to finish it on time and with complete accuracy. My supervisor appreciated my attitude and drive, and I was given several more projects after that, along with an eventual promotion and pay raise.
+            </div>
+
+            
+</div>
     )
 }
 
