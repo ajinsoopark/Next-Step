805960d22cfe561eda029f2a40bf728cbee0a6a6
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-05-09 12:00:18 -0400

    adds routes for community and star

---
+++ frontend/src/Component/CommunityGuidelines/star.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StarAnswer = () => {
+
+    return (
+        <div className='guideParent'>
+                <div className='guide'>
+                STAR
+                </div>
+        </div>
+    )
+}
+
+export default StarAnswer;
