55a524238bf31f51e702a0ee22b3a0d676354fa5
ThaiSonP <tysonpan@pursuit.org>
2019-05-13 11:38:20 -0400

    update searching

--- frontend/src/Component/Search/search.jsx
+++ frontend/src/Component/Search/search.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DisplaySearch from './displaySearch'
-// import  './displaySearch.css'
+import  './displaySearch.css'
 
 class Search extends Component {
   constructor(props){
@@ -26,40 +26,51 @@ componentDidUpdate = ()=>{
 filterThings = ()=>{
   const{filter}=this.state
   const{SearchState}=this.props.state
-  if (SearchState.data){
-
 
     let test = this.state.filter
+    let results = SearchState.data
     test = test.split(' ').map(el=>el.toLowerCase())
 
-    let results = SearchState.data
-
-    let idx = 0
-
-    while(idx < test.length){
-      results = results.filter(el=>el.question_body.toLowerCase().includes(test[idx]));
-      idx++
-    }
-    // for(i=0;i<test.length;i++){
-    //   results = results.filter(el=>el.question_body.toLowerCase().includes(test[i]))
-    // }
+    if(test.length>1){
+      for(let i=0;i<test.length;i++){
+        results = results.filter(el=>el.question_body.toLowerCase().includes(test[i]))
+      }
+      console.log(results)
+      return(results.map((el,i)=>{
+        return(
+          <div key={i} className = 'results'>
+            <section>
+            {el.username}
+            <p>{el.question_body}</p>
+            <p>{el.answer_body}</p>
+          </section>
+          </div>
+        )
+      }))
+    }else if (test.length===1 && results){
     console.log(results)
-  }
-  return null
+    return(results.map((el,i)=>{
+      return(
+        <div key={i} className = 'results'>
+          <section>
+          {el.username}
+          <p>{el.question_body}</p>
+          <p>{el.answer_body}</p>
+        </section>
+        </div>
+      )
+      }))
+    }
 }
 
   render() {
     const{filter}=this.state
     const{SearchState}=this.props.state
-    // console.log(this.state)
-    // console.log(SearchState.data)
-
 
     return(
       <div >
         Search results for {filter}
         {this.filterThings()}
-        {//<DisplaySearch search={search} data={SearchState}/>//
       }
       </div>
     )
--- frontend/src/Redux/Actions/searchActions.js
+++ frontend/src/Redux/Actions/searchActions.js
@@ -9,21 +9,10 @@ const SEARCH = "SEARCH"
 
 //This is to check if (a) user has already login in the background.
 export const onSearch = (filter) => dispatch => {
-  // search === 'questions' ?
-  // axios.get(`/api/questions/search/${filter}`).then(res => {
-  //   console.log(res)
-  //   dispatch ({
-  //     type : "SEARCH",
-  //     payload : res.data.questions
-  //   })
-  // })
-  // :
-  // axios.get(`/api/users/search/${filter}`).then(res=>{
-  //   dispatch ({
-  //     type: "SEARCH",
-  //     payload : res.data.users
-  //   })
-  // })
+  // filter=filter.split(' ')
+  // console.log(filter)
+  // filter=filter[0]
+
   axios.get(`/api/answers/search/${filter}`).then(res=>{
     // console.log(filter)
     // console.log(res.data.results)
