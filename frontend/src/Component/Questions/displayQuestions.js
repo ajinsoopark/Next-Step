2961c610a43295cac6d5c25cfd4f380d58568518
Alex <alexpark@pursuit.org>
2019-05-14 21:35:49 -0400

    aligned red check with text

--- frontend/src/Component/Questions/displayQuestions.js
+++ frontend/src/Component/Questions/displayQuestions.js
@@ -11,10 +11,10 @@ const DisplayQuestions = ({ allQuestions, selectedCategory , userAnsweredList}) 
             return(
                 <NavLink key = {i} to={"/questions/" + question.id}>
                 <div  className = "questionsParent">
-                    <div className='checkSVG'>
-                        <svg viewBox="0 0 24 24"><path d="M0 11c2.761.575 6.312 1.688 9 3.438 3.157-4.23 8.828-8.187 15-11.438-5.861 5.775-10.711 12.328-14 18.917-2.651-3.766-5.547-7.271-10-10.917z"/></svg>
-                    </div>
                     <div className='questionBody'>
+                    <span className='checkSVG'>
+                        <svg viewBox="0 0 24 24"><path d="M0 11c2.761.575 6.312 1.688 9 3.438 3.157-4.23 8.828-8.187 15-11.438-5.861 5.775-10.711 12.328-14 18.917-2.651-3.766-5.547-7.271-10-10.917z"/></svg>
+                    </span>
                     {question.question_body}
                     </div>
                 </div>
